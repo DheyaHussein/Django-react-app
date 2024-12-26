68b293de70888e8aabb61469fd5d485dc2ee4b27
DiaaHussei <117247132+DiaaHussei@users.noreply.github.com>
2024-12-27 02:33:26 +0300

    make the setting and uesr app with it models and manager

--- CoreRoot/CoreRoot/settings.py
+++ CoreRoot/CoreRoot/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #internl 
+    'core',
+    'core.user'
+    #exter
 ]
 
 MIDDLEWARE = [
@@ -98,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+AUTH_USER_MODEL = 'core_user.User'
 
 
 # Internationalization
